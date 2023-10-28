38320aafaf49dc53aadb5fe1831fcf7a791920f1
wsierz <will@sierzputowski.com>
2023-10-28 19:32:57 -0400

    Updates

--- IndoorMapper/ Processing.js
+++ IndoorMapper/ Processing.js
@@ -6,6 +6,7 @@ let destLoc;
 let path;
 let currBuilding;
 let locations;
+let mapArray;
 function loadMaps() {
     // Load all maps into memory
     mapData = {data1};
@@ -37,12 +38,35 @@ function updateMap() {
     }
 
     // if on the same floor, path
-
-
-
-    // if on diffrent floors, path to nearest stair.
-    if (currLoc.position.floor) {
-
+    if (currLoc.position.floor = destLoc.position.floor)
+    {
+        path = findPath(currLoc, destLoc);
     }
 
+    // if on diffrent floors, path to nearest stair.
+    if (currLoc.position.floor != destLoc.position.floor) {
+        // Find all available stairs, find closest stair
+
+        path = findPath(currLoc, STAIRLOC);
+    }
+
+}
+
+function createArray() {
+    mapArray = [];
+}
+
+function findPath(currLoc, destLoc) {
+    var PF = require('pathfinding');  
+    var matrix = [
+        [0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0],
+    ];
+    var grid = new PF.Grid(matrix);
+
+    var finder = new PF.AStarFinder();
+    var path = finder.findPath(1, 2, 4, 2, grid);
+
+    // turn this path into path2d 
 }
