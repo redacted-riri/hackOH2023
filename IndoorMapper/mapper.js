702a32c7b1036fb03c122c47021f4bddf37dc75e
wsierz <will@sierzputowski.com>
2023-10-29 01:26:47 -0400

    Updates

--- IndoorMapper/mapper.js
+++ IndoorMapper/mapper.js
@@ -1,22 +1,34 @@
 import React, { Component} from 'react';
 import {Platform} from 'react-native';
 import Canvas, {Image as CanvasImage, Path2D, ImageData} from 'react-native-canvas';
+import {path, findPath } from './ Processing.js'
 
 export default class Mapper extends Component {
-    handleCanvas(canvas) {
-        const ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'purple';
-        ctx.fillRect(0, 0, 100, 100);
+    handleCanvas = (canvas) => {
+
+        findPath();
+
+        const context = canvas.getContext('2d');
+
+        const drawPath = new Path2D(canvas);     
+           
+        for (let i = 1; i < path.length; i++) {
+            drawPath.lineTo(path[i][1]*10, path[i][0]*10);
+        }
+        context.fillStyle = 'red';
+        context.arc(50, 50, 49, 0, Math.PI * 2, true);
+        context.fill();
+
+        context.stroke(drawPath);
       }
     
       render() {
         if (Platform.OS === "web") {
-            return <canvas  ref={this.handleCanvas} />
+            return <canvas ref={this.handleCanvas} />
         }
         
-        return (
-            <Canvas ref={this.handleCanvas} />
-        );
+        return  <Canvas ref={this.handleCanvas} />
+        
       }
 }
 
